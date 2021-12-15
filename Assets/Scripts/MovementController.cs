12c61f97988e95ff38f162551328bb50a0ec9366
Filip Bukvic <filip.bukvic@pslib.cz>
2021-12-15 22:20:21 +0100

    Upload files

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private Transform m_WallCheck;
+
+    public PlayerController controller;
+
+    // Start is called before the first frame update
+    public float runSpeed = 20f;
+    float horizontalMove = 0f;
+    int direction = 1;
+    bool isJumping = false;
+
+    void Update()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_WallCheck.position, 0.1f, controller.m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                direction *= -1;
+                break;
+            }
+        }
+
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = direction * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping); // move player
+
+        isJumping = false;
+    }
+}
---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        IsGamePaused = false;
+        Time.timeScale = 1;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        IsGamePaused = true;
+        Time.timeScale = 0;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
